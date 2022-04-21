aedaa650780e19c3d1bea4cff911495a7a00eee6
Herb Zieger <zildj100@gmail.com>
2022-04-21 18:08:04 -0400

    Taco Bell

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -15,9 +15,10 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   SOUND
 -- -----
-LSM:Register("sound", "|cFFFF3434Owen Wilson|r", [[Interface\Addons\SharedMedia_Zildj\sound\owenWilson.mp3]])
 LSM:Register("sound", "|cFFFF3434Noice|r", [[Interface\Addons\SharedMedia_Zildj\sound\noice.mp3]])
+LSM:Register("sound", "|cFFFF3434Owen Wilson|r", [[Interface\Addons\SharedMedia_Zildj\sound\owenWilson.mp3]])
 LSM:Register("sound", "|cFFFF3434prog|r", [[Interface\Addons\SharedMedia_Zildj\sound\prog.mp3]])
+LSM:Register("sound", "|cFFFF3434Taco Bell|r", [[Interface\Addons\SharedMedia_Zildj\sound\tacobell.mp3]])
 
 -- -----
 --   STATUSBAR
