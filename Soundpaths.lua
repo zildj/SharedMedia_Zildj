edc4859883607287ca20667c4e9fdc0453788d3d
Herb Zieger <zildj100@gmail.com>
2022-06-22 01:02:17 -0400

    Jason Derulo + Arrows

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -15,6 +15,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   SOUND
 -- -----
+LSM:Register("sound", "|cFFFF3434Jason Derulo|r", [[Interface\Addons\SharedMedia_Zildj\sound\jasonDerulo.mp3]])
 LSM:Register("sound", "|cFFFF3434Noice|r", [[Interface\Addons\SharedMedia_Zildj\sound\noice.mp3]])
 LSM:Register("sound", "|cFFFF3434Owen Wilson|r", [[Interface\Addons\SharedMedia_Zildj\sound\owenWilson.mp3]])
 LSM:Register("sound", "|cFFFF3434prog|r", [[Interface\Addons\SharedMedia_Zildj\sound\prog.mp3]])
